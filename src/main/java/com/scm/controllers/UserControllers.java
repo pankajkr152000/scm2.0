23ac6176160a562614ef2314b878ba41ef1650e4
Pankaj Kumar <pankajkr152000@gmail.com>
2025-12-16 00:45:05 +0530

    get username of logged in user

--- src/main/java/com/scm/controllers/UserControllers.java
+++ src/main/java/com/scm/controllers/UserControllers.java
@@ -1,5 +1,8 @@
 package com.scm.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/user")
 public class UserControllers {
-
+    final static Logger log = LoggerFactory.getLogger(UserControllers.class);
     // user dashboard
     @RequestMapping(value="/dashboard", method=RequestMethod.GET)
     public String userDashboard() {
@@ -17,7 +20,10 @@ public class UserControllers {
 
     // user profile
     @RequestMapping(value="/profile", method=RequestMethod.GET)
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+        String loggedInUserName = UserControllersHelper.getEmailOfLoggedinUser(authentication);
+        log.info("Username of logged in user : {}", loggedInUserName);
+        
         return "user/userProfile";
     }
     
