7670f257c7a536cc1dc2f33da7660186c96a4272
Pankaj Kumar <pankajkr152000@gmail.com>
2026-01-01 22:38:57 +0530

    Request from user is received into DTO

--- src/main/java/com/scm/dto/SocialLinkDTO.java
+++ src/main/java/com/scm/dto/SocialLinkDTO.java
@@ -1,14 +1,17 @@
 package com.scm.dto;
 
+import com.scm.validation.annotation.ValidSocialLink;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ValidSocialLink
 public class SocialLinkDTO {
 
-    private String title; // Facebook, GitHub, etc.
+    private String title; // Facebook, GitHub, LinkedIn etc.
 
     @NotBlank(message = "Social link URL is required")
     private String link;
