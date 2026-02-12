0dbe64702dc16954647b35bd768d2a2cfdf70b5f
Pankaj Kumar <pankajkr152000@gmail.com>
2026-02-12 23:12:05 +0530

    unused imports removed

--- src/main/java/com/scm/repository/ISocialLinkRepository.java
+++ src/main/java/com/scm/repository/ISocialLinkRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.scm.entity.Contact;
 import com.scm.entity.SocialLink;
